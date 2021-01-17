cb6f5b0ee54162bbb643123018ffa95f153d3c90
THEKing767 <77219734+THEKing767@users.noreply.github.com>
2021-01-17 11:06:47 +0530

    Add files via upload

---
+++ competitions.php
@@ -0,0 +1,31 @@
+<?php
+    include 'header.php'
+?>
+        <style>
+            .com {
+                text-decoration: underline;
+            }
+        </style>
+        <main>
+            <section>
+                <div class="wrapper">
+                    <h1 style="font-size:40px; padding: 30px 50px;">Title for this page</h1>
+                    <p style="padding: 30px 50px; font-size: 20px" class="index-text">Paragragh for this page(random text)."Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla</p>
+                    <ul class="ml" style="margin-bottom:50px">
+                        <li><a href="dcomp.php">Design Competition</a>
+                            <ul class="dropdown">
+                                <p>hihihihi</p><br>
+                            </ul>
+                        </li><br>
+                        <li><a href="acomp.php">Art Competition</a>
+                            <ul class="dropdown">
+                                <p>hikiji</p>
+                            </ul><br>
+                        </li>
+                    </ul>
+                </div>
+            </section>
+        </main>
+<?php
+    include 'footer.php'
+?>
---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+	include 'header.php'
+?>
+		<style>
+			.hom {
+				text-decoration: underline;
+			}
+		</style>
+		<main>
+			<section class="index-banner">
+				<div class="vertical-center">
+					<h2>Title</h2>
+
+				</div>
+			</section>
+			<section>
+				<div class="wrapper">
+                    <p style="padding: 30px 50px; font-size: 20px" class="index-text">
+                    This website is aiming to spread awareness about Covid-19. Aswell as spreading awareness about this disease we want to promote taking action. <br>If you go though this website, you will learn: What Covid-19 is. How to pevent it, and What is inside of Covid-19.<br></p>  
+                </div>
+			</section>
+		</main>
+		<?php 
+			include 'footer.php';
+		?>
+	</body>
+</html>
